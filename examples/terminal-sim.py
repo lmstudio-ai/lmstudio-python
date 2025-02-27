ab918301474a5a77c969734123eb83daa32dd8fb
Alyssa Coghlan <ncoghlan@gmail.com>
2025-02-27 23:14:08 +1000

    Add docs example scripts to repo

---
+++ examples/terminal-sim.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Example script demonstrating a simulated terminal command processor."""
+
+import readline # Enables input line editing
+
+import lmstudio as lm
+
+model = lm.llm()
+console_history = []
+
+while True:
+    try:
+        user_command = input("$ ")
+    except EOFError:
+        print()
+        break
+    if user_command.strip() == "exit":
+        break
+    console_history.append(f"$ {user_command}")
+    history_prompt = "\n".join(console_history)
+    prediction_stream = model.complete_stream(
+        history_prompt,
+        config={ "stopStrings": ["$"] },
+    )
+    for fragment in prediction_stream:
+        print(fragment.content, end="", flush=True)
+    print()
+    console_history.append(prediction_stream.result().content)
